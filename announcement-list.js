dbb27c326c3a320256b77fab8126f5e1422ce6ff
Vikram Sreekanti <viksree@gmail.com>
2016-09-13 09:29:36 -0700

    Reorder announcements.

--- announcement-list.js
+++ announcement-list.js
@@ -6,15 +6,15 @@
 // an array.
 
 var announcements = {
-  "2016-08-22": [
-    "The first lecture will be on 8/24.",
-    "There will <b>not</b> be section during the week of 8/22. The first sections will be on 8/30 and 8/31.",
+  "2016-09-12": [
+    "Homework #2 released. Due Monday, 9/19 at 11:59pm."
   ],
   "2016-08-29": [
     "HW1 Released- due next Wednesday, 9/7 at 11:59pm",
     "HW0 Deadline Postponed- now due Thursday, 9/1 at 11:59am (i.e., noon!)"
   ],
-  "2016-09-12": [
-    "Homework #2 released. Due Monday, 9/19 at 11:59pm."
-  ]
+  "2016-08-22": [
+    "The first lecture will be on 8/24.",
+    "There will <b>not</b> be section during the week of 8/22. The first sections will be on 8/30 and 8/31.",
+  ],
 }
